e0d2bde2befef175329b9a16cbf5554eaf3459bc
PapercutT <eric_chagas12@outlook.com>
2019-09-29 17:19:31 -0300

    Checkpoint antes da alteracao na classe dominio Reserva

--- Trabalho 1 TP/entidades.cpp
+++ Trabalho 1 TP/entidades.cpp
@@ -1,5 +1,20 @@
 /*Biblioteca de classes Entidade*/
 #include <iostream>
+#include "dominios.h"
 
 using namespace std;
 
+class Usuario {
+
+private:
+    Nome nome;
+    Telefone telefone;
+    Email email;
+    Senha senha;
+    Cpf cpf;
+
+public:
+
+};
+
+class Reserva
