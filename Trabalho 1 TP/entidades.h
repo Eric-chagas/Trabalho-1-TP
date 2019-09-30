9d5a8e753058798fc6334f8646b65b3bec24eaeb
PapercutT <eric_chagas12@outlook.com>
2019-09-30 14:48:21 -0300

    Adicionado arquivo entidades.h

---
+++ Trabalho 1 TP/entidades.h
@@ -0,0 +1,56 @@
+#ifndef ENTIDADES_H
+#define ENTIDADES_H
+
+using namespace std;
+
+class Usuario {
+
+private:
+    Nome nome;
+    Telefone telefone;
+    Email email;
+    Senha senha;
+    Cpf cpf;
+
+public:
+
+};
+
+class Reserva {
+
+private:
+    Codigo_reserva codigo;
+    Assento assento;
+    Bagagem bagagem;
+
+public:
+};
+
+class Carona {
+
+private:
+    Codigo_carona codigo;
+    Cidade cidade_origem;
+    Estado estado_origem;
+    Cidade cidade_destino;
+    Estado estado_destino;
+    Data data;
+    Duracao duracao;
+    Vagas vagas;
+    Preco preco;
+
+public:
+};
+
+class Conta {
+
+private:
+    Codigo_banco banco;
+    Numero_agencia agencia;
+    Numero_conta numero;
+
+public:
+};
+
+
+#endif // ENTIDADES_H
